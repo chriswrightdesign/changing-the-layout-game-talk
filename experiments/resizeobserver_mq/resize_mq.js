2a2b653a693a3ea4b7ed288240235335e77fcbc2
Christopher Wright <chrisw@ChrisW-MBP-7.local>
2017-09-12 02:36:55 +1000

    Change: approach to matching media condition

--- experiments/resizeobserver_mq/resize_mq.js
+++ experiments/resizeobserver_mq/resize_mq.js
@@ -4,57 +4,40 @@ const targetComponent = document.querySelector('.js-size-target');
 
 const toInt = value => parseInt(value, 10);
 
-const setVariable = target => (varName, value) => {
-    target.style.setProperty(varName, value);
-}
-
-
-const getComputedVariable = target => variableName => {
-    const styles = getComputedStyle(targetComponent);
-    const currentStyle = styles.getPropertyValue(variableName);
-    return currentStyle;
-}
-
 const getVariable = targetElement =>
     variableName => getComputedStyle(targetElement).getPropertyValue(variableName);
 
-const getMediaCondition = (width, queryMin, queryMax) => {
-
-    const {clientWidth} = document.documentElement;
-
-    return width >= toInt(queryMin) && width <= toInt(queryMin) ? width : (toInt(clientWidth) + toInt(queryMin));
+const matchesMediaCondition = (width, queryMin, queryMax) => {
+    const matchesMin = width >= toInt(queryMin);
+    const matchesMax = width <= toInt(queryMax);
+    return matchesMin && matchesMax;
 }
 
-const elementQuery = (target, size) => {
+const containerQuery = (target, size) => {
     const {width, height} = size;
 
-    const variable = getComputedVariable(targetComponent);
+    const variable = getVariable(targetComponent);
 
     const queryClass = variable('--targetQueryClass').trim();
 
     const queryMin = variable('--targetQuerySizeMin');
 
-    const queryMax = variable('--targetQuerySizeMax')
+    const queryMax = variable('--targetQuerySizeMax');
 
-    const matchMq = getMediaCondition(width, queryMin, queryMax);
+    const matchMq = matchesMediaCondition(width, queryMin, queryMax);
 
-    if (window.matchMedia(`(min-width: ${matchMq}px)`).matches) {
-        if(!target.classList.contains(queryClass)) {
-            targetComponent.classList.add(queryClass);
-        }
-    } else {
-        targetComponent.classList.remove(qClass);
-    }
+    const action = matchMq && !target.classList.contains(queryClass) ? 'add' : 'remove';
+
+    targetComponent.classList[action](queryClass);
+
+
 }
-
-
 
 const reportSize = entry => {
     const {contentRect, target} = entry;
     const {width, height} = contentRect;
-    elementQuery(target, {width, height});
+    containerQuery(target, {width, height});
 }
-
 
 const sizeWatcher = new ResizeObserver(entries => entries.forEach(reportSize));
 
